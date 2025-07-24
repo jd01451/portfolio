d8b19bf014d00a1fad10411af8b8b4e0f379456a
jd01451 <jdp01451@gmail.com>
2025-07-24 11:46:46 +0900

    update

--- js/main.js
+++ js/main.js
@@ -1,16 +1,37 @@
 $(document).ready(function(){
-    AOS.init();
+  AOS.init();
 
-    const swiper = new Swiper('.swiper', {
+  const swiper = new Swiper('.swiper', {
+    direction: "vertical",
+    slidesPerView: 1,
+    spaceBetween: 0,
+    mousewheel: {
+      forceToAxis: true,
+      sensitivity: 5,
+      thresholdDelta: 1,
+      thresholdTime: 0
+    },
+    speed: 600,
+    on: {
+      slideChange: function () {
+        const idx = this.realIndex;
+        
+        $("header nav ul li").removeClass("on");
+        $("header nav ul li").eq(idx).addClass("on");
 
-      direction: "vertical",
-      slidesPerView: 1,
-      spaceBetween: 0,
-      mousewheel: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      
-    });
+        if (idx === 0) {
+          $("header").removeClass("on");
+        } else {
+          $("header").addClass("on");
+        }
+
+        this.update();
+      }
+    }
+  });
+
+  $("header nav ul li").click(function () {
+    const idx = $(this).index();
+    swiper.slideTo(idx);
+  });
 });
